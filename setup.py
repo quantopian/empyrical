6ceef775968183ba8c3f8edfe18c5a88944303e6
Calvin DeBoer <cdeboer@horizoninvestments.com>
2017-05-19 11:00:28 -0400

    probably should update this as well

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ requirements = [
 extras_requirements = {
     "dev": [
         "nose==1.3.7",
-        "nose-parameterized==0.5.0",
+        "parameterized==0.6.1",
         "flake8==2.5.1"
     ]
 }
